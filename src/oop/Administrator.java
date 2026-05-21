080cabf10aac9380bec10ab2e73cc3b00029efad
Matthew Pasaol <pasaolmatthew394@gmail.com>
2026-05-21 22:15:22 +0800

    Updated Administrator.java

--- src/oop/Administrator.java
+++ src/oop/Administrator.java
@@ -1,18 +1,24 @@
+package oop;
+
 public class Administrator extends Person {
     private String role;
+    private String adminId;
 
-    public Administrator() {}
-
-    public Administrator(String name, int age, String role) {
+    public Administrator(String name, int age, String adminId, String role) {
         super(name, age);
-        this.role = role;
+        this.adminId = adminId;
+        this.role    = role;
     }
+
+    public String getAdminId() { return adminId; }
+    public String getRole()    { return role; }
 
     @Override
     public void displayInfo() {
-        System.out.println("Administrator Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Role: " + role);
-        System.out.println("---------------------");
+        System.out.println("  Name    : " + name);
+        System.out.println("  Age     : " + age);
+        System.out.println("  Admin ID: " + adminId);
+        System.out.println("  Role    : " + role);
+        System.out.println("  " + "-".repeat(38));
     }
 }
