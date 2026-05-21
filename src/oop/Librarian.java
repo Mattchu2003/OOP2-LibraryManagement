9fa0d6c8526d7ba45576017f6480d7efedde1cba
Matthew Pasaol <pasaolmatthew394@gmail.com>
2026-05-21 22:20:33 +0800

    Updated Librarian.java

--- src/oop/Librarian.java
+++ src/oop/Librarian.java
@@ -1,13 +1,21 @@
 package oop;
 
 public class Librarian extends Person {
+    private String employeeId;
 
-    public Librarian(String name, int age) {
+    public Librarian(String name, int age, String employeeId) {
         super(name, age);
+        this.employeeId = employeeId;
     }
+
+    public String getEmployeeId() { return employeeId; }
 
     @Override
     public void displayInfo() {
-        System.out.println("Librarian: " + name + ", Age: " + age);
+        System.out.println("  Name       : " + name);
+        System.out.println("  Age        : " + age);
+        System.out.println("  Employee ID: " + employeeId);
+        System.out.println("  Role       : Librarian");
+        System.out.println("  " + "-".repeat(38));
     }
 }
