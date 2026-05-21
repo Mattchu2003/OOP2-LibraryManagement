0b52e9e96242d7ccc9f56eeaded04d429ba908ae
Matthew Pasaol <pasaolmatthew394@gmail.com>
2026-05-21 22:19:44 +0800

    Updated Guest.java

--- src/oop/Guest.java
+++ src/oop/Guest.java
@@ -1,11 +1,20 @@
-public class Guest extends Person {
+package oop;
 
-    public Guest(String name, int age) {
+public class Guest extends Person {
+    private String guestId;
+
+    public Guest(String name, int age, String guestId) {
         super(name, age);
+        this.guestId = guestId;
     }
+
+    public String getGuestId() { return guestId; }
 
     @Override
     public void displayInfo() {
-        System.out.println("Guest Name: " + getName() + ", Age: " + getAge());
+        System.out.println("  Name    : " + name);
+        System.out.println("  Age     : " + age);
+        System.out.println("  Guest ID: " + guestId);
+        System.out.println("  " + "-".repeat(38));
     }
 }
